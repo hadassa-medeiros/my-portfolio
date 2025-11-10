f4f3d6b26cc84b0fd2150cf05e00bca000d06a59
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2025-11-10 19:21:19 +0100

    refactor: Update landing page

--- src/lib/translator.ts
+++ src/lib/translator.ts
@@ -2,9 +2,11 @@ import type { Languages } from "./routes";
 type TranslationKeys =
   | "ABOUT"
   | "PROJECTS"
+  | "LOCATION"
   | "ABOUT_P1"
   | "ABOUT_P2"
   | "ABOUT_P3"
+  | "PLACEHOLDER_MSG"
   | "PROJECT_1_NAME"
   | "PROJECT_2_NAME"
   | "PROJECT_3_NAME"
@@ -19,24 +21,31 @@ class Translator {
 
   private translatables: Translatable = {
     es: {
+      ABOUT: "sobre mí",
       PROJECTS: "proyectos",
-      ABOUT: "sobre",
-      ABOUT_P1: "jl",
-      ABOUT_P2: "",
-      ABOUT_P3: "",
+      ABOUT_P1: "desarrolladora web full-stack (front-end focus)",
+      LOCATION: "Barcelona, Spain",
+      ABOUT_P2: "Full stack developer, front-end focus, UX awareness, academic background in Architecture and Urbanism, specialized in the MERN stack.",
+      ABOUT_P3: "I like things to flow and be uncomplicated.",
+
+      PLACEHOLDER_MSG: "en progreso",
+
       PROJECT_1_NAME: "workout tracker",
-      PROJECT_2_NAME: "forms web app",
+      PROJECT_2_NAME: "forms management platform",
       PROJECT_3_NAME: "custom extension for revit",
       PROJECT_4_NAME: "custom component library",
     },
     en: {
+      ABOUT: "about me",
       PROJECTS: "projects",
-      ABOUT: "about",
-      ABOUT_P1: "",
-      ABOUT_P2: "",
-      ABOUT_P3: "",
+      ABOUT_P1: "full-stack web developer (front-end focus)",     LOCATION: "Barcelona, Spain",
+      ABOUT_P2: "Desarrolladora full stack, con enfoque en front-end, conciencia de UX, formación académica en Arquitectura y Urbanismo, especializado en el stack MERN.",
+      ABOUT_P3: "Me gusta que las cosas fluyan y sean sencillas.",
+      
+      PLACEHOLDER_MSG: "in progress",
+
       PROJECT_1_NAME: "Momentum workout tracker",
-      PROJECT_2_NAME: "Forms web application",
+      PROJECT_2_NAME: "plataforma de gestión de formularios",
       PROJECT_3_NAME: "Bulding models management extension for Revit",
       PROJECT_4_NAME: "Custom components library",
     },
