e0524075d24811e0058076f8c3483da95e1c0a8d
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2026-07-24 00:36:26 +0200

    v0.1 — 23/07/2026

--- src/lib/translator.ts
+++ src/lib/translator.ts
@@ -2,7 +2,6 @@ import type { Languages } from "./routes";
 type TranslationKeys =
   | "ABOUT"
   | "PROJECTS"
-  | "REPOSITORY"
   | "LOCATION"
   | "ABOUT_P1"
   | "ABOUT_P2"
@@ -11,7 +10,6 @@ type TranslationKeys =
   | "PROJECT_1_NAME"
   | "PROJECT_2_NAME"
   | "PROJECT_3_NAME"
-  | "PROJECT_4_NAME";
 
 type Translatable = {
   [lang in Languages]: Record<TranslationKeys, string>;
@@ -22,51 +20,31 @@ class Translator {
 
   private translatables: Translatable = {
     ca: {
-      ABOUT: "sobre mi",
       PROJECTS: "projectes",
-      REPOSITORY: "repositori",
-      ABOUT_P1: "desenvolupadora web full-stack (front-end focus)",
+      ABOUT_P1: "estructura, textura, traducció",
       LOCATION: "Barcelona, Spain",
-      ABOUT_P2: "Desenvolupadora full stack, amb focus en front-end, consciència d'UX, formació acadèmica en Arquitectura i Urbanisme, especialitzada en l'stack MERN.",
-      ABOUT_P3: "M'agrada que les coses flueixin i siguin senzilles.",
 
       PLACEHOLDER_MSG: "en progrés",
 
-      PROJECT_1_NAME: "Momentum workout tracker",
-      PROJECT_2_NAME: "plataforma de gestió de formularis",
-      PROJECT_3_NAME: "extensió de gestió de models d'edificis per a Revit",
-      PROJECT_4_NAME: "biblioteca de components personalitzats",
+      PROJECT_1_NAME: "gestió de models d'edificis per a Revit",
     },
     es: {
-      ABOUT: "sobre mí",
       PROJECTS: "proyectos",
-      REPOSITORY: "repositorio",
-      ABOUT_P1: "desarrolladora web full-stack (front-end focus)",
+      ABOUT_P1: "estructura, textura, tradución",
       LOCATION: "Barcelona, Spain",
-      ABOUT_P2: "Full stack developer, front-end focus, UX awareness, academic background in Architecture and Urbanism, specialized in the MERN stack.",
-      ABOUT_P3: "I like things to flow and be uncomplicated.",
 
       PLACEHOLDER_MSG: "en progreso",
 
-      PROJECT_1_NAME: "workout tracker",
-      PROJECT_2_NAME: "forms management platform",
-      PROJECT_3_NAME: "custom extension for revit",
-      PROJECT_4_NAME: "custom component library",
+      PROJECT_1_NAME: "gestión de modelos de edificaciones para Revit",
     },
     en: {
-      ABOUT: "about me",
       PROJECTS: "projects",
-      REPOSITORY: "repository",
-      ABOUT_P1: "full-stack web developer (front-end focus)",     LOCATION: "Barcelona, Spain",
-      ABOUT_P2: "Desarrolladora full stack, con enfoque en front-end, conciencia de UX, formación académica en Arquitectura y Urbanismo, especializado en el stack MERN.",
-      ABOUT_P3: "Me gusta que las cosas fluyan y sean sencillas.",
+      ABOUT_P1: "structure, texture, translation",
+      LOCATION: "Barcelona, Spain",
       
       PLACEHOLDER_MSG: "in progress",
 
-      PROJECT_1_NAME: "Momentum workout tracker",
-      PROJECT_2_NAME: "plataforma de gestión de formularios",
-      PROJECT_3_NAME: "Bulding models management extension for Revit",
-      PROJECT_4_NAME: "Custom components library",
+      PROJECT_1_NAME: "",
     },
   };
 
